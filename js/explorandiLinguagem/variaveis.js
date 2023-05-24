5a4c3773c8f068a6e7c5b09fea741d3cfe19b11b
Giovanna Vilela <giovannavileladeoliveira@gmail.com>
2023-05-24 16:50:45 -0300

    estruturação do HTML com bootstrap

---
+++ js/explorandiLinguagem/variaveis.js
@@ -0,0 +1,5 @@
+console.log("olá mundo");
+//js é case sensitive
+const idade = 26;// declarar constante 
+let peso = 80;// declarar variaveis com boas praticas
+// n declare sem nada, ele permite mas pode dar problema
